c36de201ef5e75ea01cfd35642421a4158e43781
VictorHugo <victor.hugo@newpost.com>
2021-08-25 15:20:19 -0300

    arrumando exercicio proposto na aula

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    static int a = 30;
+    static short b = 20;
+    static long c = 30;
+    static int d = a + b;
+    
+    public static void main(String[] args) {
+        imp(d);
+    }
+    
+    public static void imp(int x){
+        System.out.println(x);        
+    }
+	
+}
+
+
